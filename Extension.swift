d2269f7b12db01b47b9c65f1ce5002362060f918
Muhammad Abdullah Sadaqat <abdullahsheikh18@gmail.com>
2022-05-15 18:39:41 +0500

    Update Extension.swift

--- Extension.swift
+++ Extension.swift
@@ -1,1 +1,428 @@
 
+
+import Foundation
+import UIKit
+
+extension StringProtocol {
+    var lines: [SubSequence] { split(whereSeparator: \.isNewline) }
+    var removingAllExtraNewLines: String { lines.joined(separator: "\n") }
+}
+extension Dictionary {
+    subscript(i: Int) -> (key: Key, value: Value) {
+        return self[index(startIndex, offsetBy: i)]
+    }
+}
+
+extension Sequence where Element: Hashable {
+    func uniqued() -> [Element] {
+        var set = Set<Element>()
+        return filter { set.insert($0).inserted }
+    }
+}
+
+extension UITableViewCell {
+    func enable(on: Bool) {
+        self.isUserInteractionEnabled = on
+        for view in contentView.subviews {
+            self.isUserInteractionEnabled = on
+            view.alpha = on ? 1 : 0.5
+        }
+    }
+}
+
+extension String {
+
+    func localized() -> String{
+        
+        let path = Bundle.main.path(forResource: LANGUAGE, ofType: "lproj")
+        let bundle = Bundle(path: path!)
+        
+        return NSLocalizedString(self, tableName: nil, bundle: bundle!, value: "", comment: "")
+    }
+}
+
+extension UICollectionViewCell {
+    func isEnabled (on: Bool) {
+//        self.isUserInteractionEnabled = on
+        for view in contentView.subviews {
+//            self.isUserInteractionEnabled = on
+            view.alpha = on ? 1 : 0.5
+        }
+    }
+}
+
+extension Sequence {
+    public func toDictionary<Key: Hashable>(with selectKey: (Iterator.Element) -> Key) -> [Key:Iterator.Element] {
+        var dict: [Key:Iterator.Element] = [:]
+        for element in self {
+            dict[selectKey(element)] = element
+        }
+        return dict
+    }
+}
+
+extension NumberFormatter {
+
+    static let moneyFormatter: NumberFormatter = {
+        let nf = NumberFormatter()
+        nf.generatesDecimalNumbers = true
+        nf.maximumFractionDigits = 2
+        nf.minimumFractionDigits = 2
+        nf.numberStyle = .decimal
+        return nf
+    }()
+
+    static func resetupCashed() {
+        moneyFormatter.locale = Locale.current
+    }
+}
+
+
+extension DateFormatter {
+
+    static let dobFormatter: DateFormatter = {
+        let df = DateFormatter()
+        df.dateStyle = .full
+        return df
+    }()
+
+    static func resetupCashed() {
+        dobFormatter.locale = Locale.current
+    }
+}
+
+extension Dictionary {
+    func nullKeyRemoval() -> Dictionary {
+        var dict = self
+
+        let keysToRemove = Array(dict.keys).filter { dict[$0] is NSNull }
+        for key in keysToRemove {
+            dict.removeValue(forKey: key)
+        }
+
+        return dict
+    }
+}
+extension String {
+    func getStringHeight(constraintedWidth width: CGFloat, font: UIFont) -> CGFloat {
+        let label =  UILabel(frame: CGRect(x: 0, y: 0, width: width, height: .greatestFiniteMagnitude))
+        label.numberOfLines = 0
+        label.text = self
+        label.font = font
+        label.sizeToFit()
+        
+        return label.frame.height
+    }
+    func replace(string:String, replacement:String) -> String {
+        return self.replacingOccurrences(of: string, with: replacement, options: NSString.CompareOptions.literal, range: nil)
+    }
+
+    func removeWhitespace() -> String {
+        return self.replace(string: " ", replacement: "")
+    }
+    var removeSpecialCharacters: String {
+        let okayChars = Set("-0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ_abcdefghijklmnopqrstuvwxyz")
+        return self.filter {okayChars.contains($0) }
+    }
+}
+
+extension UIColor {
+    
+    func colorForHax(_ rgba:String)->UIColor{
+        var red:   CGFloat = 0.0
+        var green: CGFloat = 0.0
+        var blue:  CGFloat = 0.0
+        var alpha: CGFloat = 1.0
+        
+        if rgba.hasPrefix("#") {
+            let index   = rgba.index(rgba.startIndex, offsetBy: 1)
+            let hex     = rgba.substring(from: index)
+            let scanner = Scanner(string: hex)
+            var hexValue: CUnsignedLongLong = 0
+            if scanner.scanHexInt64(&hexValue) {
+                switch (hex.count) {
+                case 3:
+                    red   = CGFloat((hexValue & 0xF00) >> 8)       / 15.0
+                    green = CGFloat((hexValue & 0x0F0) >> 4)       / 15.0
+                    blue  = CGFloat(hexValue & 0x00F)              / 15.0
+                    break
+                case 4:
+                    red   = CGFloat((hexValue & 0xF000) >> 12)     / 15.0
+                    green = CGFloat((hexValue & 0x0F00) >> 8)      / 15.0
+                    blue  = CGFloat((hexValue & 0x00F0) >> 4)      / 15.0
+                    alpha = CGFloat(hexValue & 0x000F)             / 15.0
+                    break
+                case 6:
+                    red   = CGFloat((hexValue & 0xFF0000) >> 16)   / 255.0
+                    green = CGFloat((hexValue & 0x00FF00) >> 8)    / 255.0
+                    blue  = CGFloat(hexValue & 0x0000FF)           / 255.0
+                    break
+                case 8:
+                    red   = CGFloat((hexValue & 0xFF000000) >> 24) / 255.0
+                    green = CGFloat((hexValue & 0x00FF0000) >> 16) / 255.0
+                    blue  = CGFloat((hexValue & 0x0000FF00) >> 8)  / 255.0
+                    alpha = CGFloat(hexValue & 0x000000FF)         / 255.0
+                    break
+                default:
+                    print("Invalid RGB string, number of characters after '#' should be either 3, 4, 6 or 8", terminator: "")
+                    break
+                }
+            } else {
+                print("Scan hex error")
+            }
+        } else {
+            print("Invalid RGB string, missing '#' as prefix", terminator: "")
+        }
+        
+        return UIColor(red: red, green: green, blue: blue, alpha: alpha)
+    }
+    
+    
+}
+
+extension NSObject {
+    class var id: String {
+        return String(describing: self)
+    }
+}
+
+extension Date {
+  /// Returns the amount of years from another date
+  func years(from date: Date) -> Int {
+    return Calendar.current.dateComponents([.year], from: date, to: self).year ?? 0
+  }
+  /// Returns the amount of months from another date
+  func months(from date: Date) -> Int {
+    return Calendar.current.dateComponents([.month], from: date, to: self).month ?? 0
+  }
+  /// Returns the amount of weeks from another date
+  func weeks(from date: Date) -> Int {
+    return Calendar.current.dateComponents([.weekOfYear], from: date, to: self).weekOfYear ?? 0
+  }
+  /// Returns the amount of days from another date
+  func days(from date: Date) -> Int {
+    return Calendar.current.dateComponents([.day], from: date, to: self).day ?? 0
+  }
+  /// Returns the amount of hours from another date
+  func hours(from date: Date) -> Int {
+    return Calendar.current.dateComponents([.hour], from: date, to: self).hour ?? 0
+  }
+  /// Returns the amount of minutes from another date
+  func minutes(from date: Date) -> Int {
+    return Calendar.current.dateComponents([.minute], from: date, to: self).minute ?? 0
+  }
+  /// Returns the amount of seconds from another date
+  func seconds(from date: Date) -> Int {
+    return Calendar.current.dateComponents([.second], from: date, to: self).second ?? 0
+  }
+  /// Returns the a custom time interval description from another date
+  func offset(from date: Date) -> String {
+    if years(from: date)  > 0 { return "\(years(from: date))y"  }
+    if months(from: date) > 0 { return "\(months(from: date))M" }
+    if weeks(from: date)  > 0 { return "\(weeks(from: date))w"  }
+    if days(from: date)  > 0 { return "\(days(from: date))d"  }
+    if hours(from: date)  > 0 { return "\(hours(from: date))h"  }
+    if minutes(from: date) > 0 { return "\(minutes(from: date))m" }
+    if seconds(from: date) > 0 { return "\(seconds(from: date))s" }
+    return ""
+  }
+}
+
+
+extension UIView {
+    func onClickView(target: Any, _ selector: Selector) {
+        isUserInteractionEnabled = true
+        let tap = UITapGestureRecognizer(target: target, action: selector)
+        addGestureRecognizer(tap)
+    }
+}
+
+extension UIView {
+
+    
+  // OUTPUT 1
+  func dropShadoeLeftBottom(scale: Bool = true) {
+    layer.masksToBounds = false
+    layer.shadowColor = UIColor.black.cgColor
+    layer.shadowOpacity = 0.5
+    layer.shadowOffset = CGSize(width: -1, height: 1)
+    layer.shadowRadius = 1
+
+    layer.shadowPath = UIBezierPath(rect: bounds).cgPath
+    layer.shouldRasterize = true
+    layer.rasterizationScale = scale ? UIScreen.main.scale : 1
+  }
+
+  // OUTPUT 2
+  func dropShadowAllSide(color: UIColor, opacity: Float = 0.5, offSet: CGSize, radius: CGFloat = 1, scale: Bool = true) {
+    layer.masksToBounds = false
+    layer.shadowColor = color.cgColor
+    layer.shadowOpacity = opacity
+    layer.shadowOffset = offSet
+    layer.shadowRadius = radius
+
+    layer.shadowPath = UIBezierPath(rect: self.bounds).cgPath
+    layer.shouldRasterize = true
+    layer.rasterizationScale = scale ? UIScreen.main.scale : 1
+  }
+}
+extension Array where Element : Equatable {
+    var unique: [Element] {
+        var uniqueValues: [Element] = []
+        forEach { item in
+            if !uniqueValues.contains(item) {
+                uniqueValues += [item]
+            }
+        }
+        return uniqueValues
+    }
+}
+
+extension UIView {
+    
+    @IBInspectable var cornerRadiusV: CGFloat {
+        get {
+            return layer.cornerRadius
+        }
+        set {
+            layer.cornerRadius = newValue
+            layer.masksToBounds = newValue > 0
+        }
+    }
+
+    @IBInspectable var borderWidthV: CGFloat {
+        get {
+            return layer.borderWidth
+        }
+        set {
+            layer.borderWidth = newValue
+        }
+    }
+
+    @IBInspectable var borderColorV: UIColor? {
+        get {
+            return UIColor(cgColor: layer.borderColor!)
+        }
+        set {
+            layer.borderColor = newValue?.cgColor
+        }
+    }
+}
+
+extension UICollectionView {
+
+    func setEmptyMessage(_ message: String) {
+        let messageLabel = UILabel(frame: CGRect(x: 0, y: 0, width: self.bounds.size.width, height: self.bounds.size.height))
+        messageLabel.text = message
+        messageLabel.textColor = .black
+        messageLabel.numberOfLines = 0
+        messageLabel.textAlignment = .center
+        messageLabel.font = UIFont(name: "TrebuchetMS", size: 15)
+        messageLabel.sizeToFit()
+
+        self.backgroundView = messageLabel
+//        self.separatorStyle = .none
+    }
+    
+    func restore() {
+        self.backgroundView = nil
+//        self.separatorStyle = .singleLine
+    }
+
+}
+
+extension UITableView {
+    func setEmptyMessageForTbl(_ message: String) {
+        let messageLabel = UILabel(frame: CGRect(x: 0, y: 0, width: self.bounds.size.width, height: self.bounds.size.height))
+        messageLabel.text = message
+        messageLabel.textColor = .black
+        messageLabel.numberOfLines = 0
+        messageLabel.textAlignment = .center
+        messageLabel.font = UIFont(name: "TrebuchetMS", size: 15)
+        messageLabel.sizeToFit()
+
+        self.backgroundView = messageLabel
+//        self.separatorStyle = .none
+    }
+    
+    func restoreForTbl() {
+        self.backgroundView = nil
+//        self.separatorStyle = .singleLine
+    }
+
+}
+
+
+func randomString(length: Int) -> String {
+  let letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+  return String((0..<length).map{ _ in letters.randomElement()! })
+}
+
+extension UIView {
+   func roundCorners(corners: UIRectCorner, radius: CGFloat) {
+        let path = UIBezierPath(roundedRect: bounds, byRoundingCorners: corners, cornerRadii: CGSize(width: radius, height: radius))
+        let mask = CAShapeLayer()
+        mask.path = path.cgPath
+        layer.mask = mask
+    }
+}
+extension UILabel {
+      func addBlurrEffect() {
+          let blurEffectView = ASBlurEffectView() // creating a blur effect view
+          blurEffectView.intensity = 1 // setting blur intensity from 0.1 to 10
+          self.addSubview(blurEffectView) // adding blur effect view as a subview to your view in which you want to use
+      }
+    
+      func removeBlurEffect() {
+          for subview in self.subviews {
+              if subview is UIVisualEffectView {
+                  subview.removeFromSuperview()
+              }
+          }
+      }
+    }
+extension UIImageView {
+    func onClick(target: Any, _ selector: Selector) {
+        isUserInteractionEnabled = true
+        let tap = UITapGestureRecognizer(target: target, action: selector)
+        addGestureRecognizer(tap)
+    }
+}
+extension UIStackView {
+    func onClick(target: Any, _ selector: Selector) {
+        isUserInteractionEnabled = true
+        let tap = UITapGestureRecognizer(target: target, action: selector)
+        addGestureRecognizer(tap)
+    }
+}
+//extension UIView {
+//      func addBlurrEffect() {
+//          let blurEffectView = ASBlurEffectView() // creating a blur effect view
+//          blurEffectView.intensity = 1 // setting blur intensity from 0.1 to 10
+//          self.addSubview(blurEffectView) // adding blur effect view as a subview to your view in which you want to use
+//      }
+//
+//      func removeBlurEffect() {
+//          for subview in self.subviews {
+//              if subview is UIVisualEffectView {
+//                  subview.removeFromSuperview()
+//              }
+//          }
+//      }
+//    }
+
+//extension UIImageView {
+//      func addBlurrEffect() {
+//          let blurEffectView = ASBlurEffectView() // creating a blur effect view
+//          blurEffectView.intensity = 1 // setting blur intensity from 0.1 to 10
+//          self.addSubview(blurEffectView) // adding blur effect view as a subview to your view in which you want to use
+//      }
+//
+//      func removeBlurEffect() {
+//          for subview in self.subviews {
+//              if subview is UIVisualEffectView {
+//                  subview.removeFromSuperview()
+//              }
+//          }
+//      }
+//    }
